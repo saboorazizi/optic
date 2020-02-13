adcc46528c6a536046c9066c9d4ea07577a7d06e
Abdul Azizi <saboorazizi.82@gmail.com>
2020-02-12 22:16:44 -0800

    commit002

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoogse = require('mongoose');
+
+const UserSchema = new mongoogse.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoogse.model('user', UserSchema);
